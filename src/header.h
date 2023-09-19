9b3ed2d2bdee0e1060a3fdb8170e69d6aa1321c0
DeepBlue <yeabsira.fikadu@aastustudent.edu.et>
2023-09-19 13:35:08 +0300

    header file

---
+++ src/header.h
@@ -0,0 +1,148 @@
+#ifndef LMS
+#define LMS
+
+#include <mysql/mysql.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <ctime>
+#include <limits>
+
+
+
+using namespace std;
+
+struct connection_details
+{
+    string server= "sql11.freesqldatabase.com";
+    string user = "sql11647459";
+    string password = "ZLGYw14be4";
+    string database = "sql11647459";
+};
+
+struct libraryMember
+{
+    string id;
+    string first_name;
+    string last_name;
+};
+
+
+struct result
+{
+    vector<vector<string>>row;
+    vector<string> column;
+    vector<int> columntype;
+    int num_row=0;
+    int num_colum=0;
+
+    bool dispaly_all()
+    {
+        int choice;
+        if(num_colum>0 && num_row>0)
+        {
+            for (size_t i = 0; i < num_row; i++)
+            {
+                for (size_t j = 0; j < num_colum; j++)
+                {
+                    cout << column[j] << " \t:\t " << row[i][j] << endl;
+                } 
+            }
+            cout << "0.back" << endl;
+            if (!(cin>>choice))
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }  
+            return true;
+        }
+        else
+        {
+            cout << "NOT FOUND!!" << endl;
+            if (!(cin>>choice))
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            return false;
+        }
+
+    }
+    bool dispaly_all_row()
+    {
+        int choice;
+        if(num_colum>0 && num_row>0)
+        {
+            for (size_t i = 0; i < num_row; i++)
+            {
+                cout << i+1 << ".";
+                for (size_t j = 0; j < num_colum; j++)
+                {
+                    cout << row[i][j] << " ";
+                } 
+                cout << endl;
+            }
+            cout << "0.back" << endl;
+            return true;
+        }
+        else
+        {
+            cout << "NOT FOUND!!" << endl;
+            if (!(cin>>choice))
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            return false;
+        }
+    }
+    bool dispaly_one(int index)
+    {
+        int choice;
+        if(num_colum>0 && num_row>0)
+        {
+            for (size_t j = 1; j < num_colum; j++)
+            {
+                cout << column[j] << " \t:\t " << row[index][j] << endl;
+            } 
+            cout << "0.back" << endl;
+            if (!(cin>>choice))
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+             
+            return true;
+        }
+        else
+        {
+            cout << "NOT FOUND!!" << endl;
+            if (!(cin>>choice))
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            return false;
+        }
+
+    }
+
+};
+
+result perform_query(string sql_query);
+string get_id(string mysql_function);
+
+void library_member();
+void recommendation();
+void browse(libraryMember libmem);
+void search();
+void browse_gener();
+void browse_auther();
+void search_by_id();
+void search_by_title();
+void search_by_isbn();
+void search_by_author();
+
+
+#endif
